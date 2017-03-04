8472c134e5f4798554efae2c1e850c00cccfddc0
Dang Fan <dangfan@fayfans.com>
2017-03-04 17:51:32 +0800

    add assembly

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,5 @@
 logLevel := Level.Warn
+
+resolvers += Resolver.url("bintray-sbt-plugins", url("http://dl.bintray.com/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
+
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.4")
--- src/main/scala/tech/ffs/hsmsimulator/Application.scala
+++ src/main/scala/tech/ffs/hsmsimulator/Application.scala
@@ -4,7 +4,7 @@ import akka.actor.{ActorSystem, Props}
 
 object Application extends App {
   override def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem("smtpserver")
+    implicit val system = ActorSystem("hsm-simulator")
     implicit val executor = system.dispatcher
 
     system.actorOf(Props[Hsm], "hsm")
